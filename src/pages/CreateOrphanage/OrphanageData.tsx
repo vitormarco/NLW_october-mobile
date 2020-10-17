b7aed50a2934f15b206fffa103df822de7e50848
Vítor Marco <vitoradrianomarco@gmail.com>
2020-10-17 18:14:17 -0300

    Finalizando app

--- src/pages/CreateOrphanage/OrphanageData.tsx
+++ src/pages/CreateOrphanage/OrphanageData.tsx
@@ -1,9 +1,97 @@
-import React from 'react';
-import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { ScrollView, View, StyleSheet, Switch, Text, TextInput, TouchableOpacity, Image } from 'react-native';
 import { Feather } from '@expo/vector-icons';
 import { RectButton } from 'react-native-gesture-handler';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import * as ImagePicker from 'expo-image-picker';
+import api from '../../services/api';
+
+interface OrphanageDataRouteParams {
+  position: {
+    latitude: number,
+    longitude: number,
+  }
+}
 
 export default function OrphanageData() {
+  const [form, setForm] = useState({
+    name: '',
+    about: '',
+    instructions: '',
+    opening_hours: '',
+    open_on_weekends: true,
+  });
+  const [images, setImages] = useState<string[]>([]);
+
+  const navigation = useNavigation();
+  const route = useRoute();
+  const params = route.params as OrphanageDataRouteParams;
+
+  const handlerCreateOrphanage = async () => {
+    const { latitude, longitude } = params.position;
+    const {
+      name,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends, 
+    } = form
+
+    console.log({
+      name,
+      about,
+      instructions,
+      opening_hours,
+      open_on_weekends,
+      latitude,
+      longitude 
+    });
+
+    const data = new FormData(); 
+    data.append('name', name);
+    data.append('about', about);
+    data.append('instructions', instructions);
+    data.append('open_on_weekends', String(open_on_weekends));
+    data.append('opening_hours', opening_hours);
+    data.append('latitude', String(latitude));
+    data.append('longitude', String(longitude));
+
+    images.forEach((image, index) => {
+      data.append('images', {
+        name: `image_${index}.jpg`,
+        type: 'image/jpg',
+        uri: image,
+      } as any);
+    });
+
+    await api.post('orphanages', data);
+    
+    navigation.navigate('OrphanagesMap');
+  }
+
+  const handlerSelectImages = async () => {
+    const { status } = await ImagePicker.requestCameraRollPermissionsAsync();
+
+    if (status !== 'granted') {
+      alert('Eita, precisamos de acesso às suas fotos...');
+      return;
+    }
+
+    const result = await ImagePicker.launchImageLibraryAsync({
+      allowsEditing: true,
+      quality: 1,
+      mediaTypes: ImagePicker.MediaTypeOptions.Images,
+    });
+
+    if (result.cancelled) {
+      return;
+    }
+
+    const { uri: image } = result;
+
+    setImages([...images, image]);
+  }
+
   return (
     <ScrollView style={styles.container} contentContainerStyle={{ padding: 24 }}>
       <Text style={styles.title}>Dados</Text>
@@ -11,21 +99,38 @@ export default function OrphanageData() {
       <Text style={styles.label}>Nome</Text>
       <TextInput
         style={styles.input}
+        value={form.name}
+        onChangeText={text => setForm({...form, name: text})}
       />
 
       <Text style={styles.label}>Sobre</Text>
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={form.about}
+        onChangeText={text => setForm({...form, about: text})}
       />
 
-      <Text style={styles.label}>Whatsapp</Text>
+      {/* <Text style={styles.label}>Whatsapp</Text>
       <TextInput
         style={styles.input}
-      />
+      /> */}
 
       <Text style={styles.label}>Fotos</Text>
-      <TouchableOpacity style={styles.imagesInput} onPress={() => {}}>
+
+      <View style={styles.uploadedImagesContainer}>
+        {images.map(image => {
+          return (
+            <Image
+              key={image}
+              source={{uri: image}}
+              style={styles.uploadedImage}
+            />
+          );
+        })}
+      </View>
+
+      <TouchableOpacity style={styles.imagesInput} onPress={handlerSelectImages}>
         <Feather name="plus" size={24} color="#15B6D6" />
       </TouchableOpacity>
 
@@ -35,11 +140,15 @@ export default function OrphanageData() {
       <TextInput
         style={[styles.input, { height: 110 }]}
         multiline
+        value={form.instructions}
+        onChangeText={text => setForm({...form, instructions: text})}
       />
 
       <Text style={styles.label}>Horario de visitas</Text>
       <TextInput
         style={styles.input}
+        value={form.opening_hours}
+        onChangeText={text => setForm({...form, opening_hours: text})}
       />
 
       <View style={styles.switchContainer}>
@@ -47,10 +156,12 @@ export default function OrphanageData() {
         <Switch 
           thumbColor="#fff" 
           trackColor={{ false: '#ccc', true: '#39CC83' }}
+          value={form.open_on_weekends}
+          onValueChange={value => setForm({...form, open_on_weekends: value})}
         />
       </View>
 
-      <RectButton style={styles.nextButton} onPress={() => {}}>
+      <RectButton style={styles.nextButton} onPress={handlerCreateOrphanage}>
         <Text style={styles.nextButtonText}>Cadastrar</Text>
       </RectButton>
     </ScrollView>
@@ -93,6 +204,19 @@ const styles = StyleSheet.create({
     paddingHorizontal: 24,
     marginBottom: 16,
     textAlignVertical: 'top',
+  },
+
+  uploadedImagesContainer: {
+    flexDirection: 'row',
+
+  },
+
+  uploadedImage: {
+    width: 64,
+    height: 64,
+    borderRadius: 20,
+    marginBottom: 32,
+    marginRight: 8,
   },
 
   imagesInput: {
--- src/pages/OrphanagesMap.tsx
+++ src/pages/OrphanagesMap.tsx
@@ -2,7 +2,7 @@ import React, {useEffect, useCallback, useState} from 'react';
 import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView, { Marker, Callout , PROVIDER_GOOGLE } from 'react-native-maps';
 import { Feather } from '@expo/vector-icons';
-import { useNavigation } from '@react-navigation/native';
+import { useNavigation, useFocusEffect } from '@react-navigation/native';
 
 import mapMarker from '../images/map-marker.png';
 import { RectButton } from 'react-native-gesture-handler';
@@ -31,9 +31,9 @@ export default function OrphanagesMap() {
     }
   }, []);
 
-  useEffect(() => {
+  useFocusEffect(() => {
     getAllOrphanages();
-  }, [getAllOrphanages]);
+  });
 
   return (
     <View style={styles.container}>
